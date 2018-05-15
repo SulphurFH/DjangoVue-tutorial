804ae6fdd83ec607c88e4ee2851e4345f9c93f20
FangHao <fanghao@FangHaos-MacBook-Pro.local>
2018-05-15 13:21:09 +0800

    post forigen&serializer detail

--- apps/books/serializers.py
+++ apps/books/serializers.py
@@ -12,11 +12,16 @@ class BooksCategorySerializer(serializers.ModelSerializer):
 
 
 class BooksSerializer(serializers.ModelSerializer):
-    # TODO 没法外键显示
     # category = BooksCategorySerializer(read_only=True)
+    category_name = serializers.ReadOnlyField(source='category.name')
+    category_type = serializers.ReadOnlyField(source='category.category_type')
     created_time = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", required=False, read_only=True)
     last_update = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", required=False, read_only=True)
 
     class Meta:
         model = Books
         fields = '__all__'
+
+    # TODO 自定义设置create
+    # def create(self, validated_data):
+    #     pass
